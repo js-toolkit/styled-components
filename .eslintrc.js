07b7481b54e4592a766c5c4b09c609f31a5207f4
Vladimir <vzh.inbox@gmail.com>
2020-07-16 17:50:37 +0600

    Update deps and configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: require.resolve('@vzh/configs/eslint/ts.react.eslintrc.js'),
+  extends: require.resolve('@vzh/configs/eslint/react'),
   rules: {
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',

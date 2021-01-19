eff3e3aa1b59f716cb138726fad60087b63ddc8a
Vladimir <vzh.inbox@gmail.com>
2021-01-19 10:20:16 +0600

    Add WatermarkField

---
+++ src/WatermarkField/WatermarkField.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export interface WatermarkFieldProps extends React.SVGAttributes<SVGSVGElement> {
+  text: string;
+  textWidth: number;
+  textHeight: number;
+}
+
+export default function WatermarkField({
+  text,
+  textWidth,
+  textHeight,
+  patternUnits,
+  patternContentUnits,
+  patternTransform,
+  ...rest
+}: WatermarkFieldProps): JSX.Element {
+  return (
+    <svg width="100%" height="100%" fill="currentColor" {...rest}>
+      <defs>
+        <pattern
+          id="textstripe"
+          width={textWidth * 2}
+          height={textHeight * 2}
+          patternUnits={patternUnits || 'userSpaceOnUse'}
+          patternContentUnits={patternContentUnits}
+          patternTransform={patternTransform || 'rotate(-45)'}
+        >
+          <text x={0} y={0} dy={textHeight}>
+            {text}
+          </text>
+          <text x={textWidth} y={textHeight} dy={textHeight}>
+            {text}
+          </text>
+        </pattern>
+      </defs>
+      <rect width="100%" height="100%" fill="url(#textstripe)" />
+    </svg>
+  );
+}
---
+++ src/WatermarkField/index.ts
@@ -0,0 +1,2 @@
+export { default } from './WatermarkField';
+export * from './WatermarkField';

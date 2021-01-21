a36720ab1d6735c927e1c521858581a546cae5e3
Vladimir <vzh.inbox@gmail.com>
2021-01-21 12:39:26 +0600

    Improve WatermarkField

--- src/WatermarkField/WatermarkField.tsx
+++ src/WatermarkField/WatermarkField.tsx
@@ -26,10 +26,22 @@ export default function WatermarkField({
           patternContentUnits={patternContentUnits}
           patternTransform={patternTransform ?? 'rotate(-45)'}
         >
-          <text x={0} y={0} dy={textHeight}>
+          <text
+            x={0}
+            y={0}
+            dy={textHeight}
+            lengthAdjust="spacingAndGlyphs"
+            dominantBaseline="text-after-edge"
+          >
             {text}
           </text>
-          <text x={textWidth} y={textHeight} dy={textHeight}>
+          <text
+            x={textWidth}
+            y={textHeight}
+            dy={textHeight}
+            lengthAdjust="spacingAndGlyphs"
+            dominantBaseline="text-after-edge"
+          >
             {text}
           </text>
         </pattern>

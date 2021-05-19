59a1773234ed90136d76e3f088a40ca0214b5116
Vladimir <vzh.inbox@gmail.com>
2021-05-19 18:55:12 +0600

    Refactor transition

--- src/HideableFlex/HideableFlex.tsx
+++ src/HideableFlex/HideableFlex.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import makeStyles from '@material-ui/styles/makeStyles';
 import { Flex, DefaultComponentType, FlexAllProps } from 'reflexy';
 import useRefCallback from '@js-toolkit/react-hooks/useRefCallback';
+import useGetSetState from 'react-use/esm/useGetSetState';
 
 const useStyles = makeStyles({
   root: ({
@@ -19,14 +20,14 @@ const useStyles = makeStyles({
         visibility: 'hidden',
         opacity: 0,
         maxHeight: collapsable ? 0 : undefined,
-        transition: `visibility 0.01ms ${transitionTimingFunction} ${transitionDuration}, max-height ${transitionDuration} ${transitionTimingFunction}, opacity ${transitionDuration} ${transitionTimingFunction}`,
+        transition: `visibility 0.1ms ${transitionTimingFunction} ${transitionDuration}, max-height ${transitionDuration} ${transitionTimingFunction}, opacity ${transitionDuration} ${transitionTimingFunction}`,
       };
     }
     return {
       visibility: 'visible',
       opacity: 1,
       maxHeight: collapsable ? '100vh' : undefined,
-      transition: `visibility 0.01ms ${transitionTimingFunction} 0s, max-height ${transitionDuration} ${transitionTimingFunction}, opacity ${transitionDuration} ${transitionTimingFunction}`,
+      transition: `visibility 0.1ms ${transitionTimingFunction} 0s, max-height ${transitionDuration} ${transitionTimingFunction}, opacity ${transitionDuration} ${transitionTimingFunction}`,
     };
   },
 });
@@ -39,6 +40,7 @@ export type HideableFlexProps<C extends React.ElementType = DefaultComponentType
     hidden?: boolean;
     disposable?: boolean;
     collapsable?: boolean;
+    keepChildren?: boolean;
     hiddenClassName?: string;
     onHidden?: VoidFunction;
     onShown?: VoidFunction;
@@ -48,6 +50,7 @@ export default function HideableFlex<C extends React.ElementType = DefaultCompon
   hidden,
   disposable,
   collapsable,
+  keepChildren,
   className,
   hiddenClassName,
   transitionDuration,
@@ -56,28 +59,46 @@ export default function HideableFlex<C extends React.ElementType = DefaultCompon
   onShown,
   ...rest
 }: HideableFlexProps<C>): JSX.Element | null {
-  const css = useStyles({
-    classes: { root: className },
-    hidden,
-    collapsable,
-    transitionDuration,
-    transitionTimingFunction,
+  const { onTransitionEnd, children: childrenProp } = rest as React.HTMLAttributes<Element>;
+
+  const [getState, setState] = useGetSetState({
+    disposed: disposable ? !!hidden : false,
+    lastChildren: childrenProp,
   });
-
-  const [disposed, setDisposed] = useState(disposable ? hidden : false);
-
-  const { onTransitionEnd } = rest as React.HTMLAttributes<Element>;
 
   const transitionEndHandler = useRefCallback<React.TransitionEventHandler>((event) => {
     onTransitionEnd && onTransitionEnd(event);
     if (disposable && event.propertyName === 'visibility') {
-      if (hidden && !disposed) setDisposed(true);
-      else if (!hidden && disposed) setDisposed(false);
+      const { disposed } = getState();
+      if (hidden && !disposed) setState({ disposed: true });
+      else if (!hidden && disposed) setState({ disposed: false });
     }
     if (event.propertyName === 'opacity') {
-      if (hidden) onHidden && onHidden();
-      else onShown && onShown();
+      if (hidden) {
+        keepChildren && setState({ lastChildren: undefined });
+        onHidden && onHidden();
+      } else {
+        keepChildren && setState({ lastChildren: childrenProp });
+        onShown && onShown();
+      }
     }
+  });
+
+  useEffect(() => {
+    if (disposable && !hidden && getState().disposed) {
+      setState({ disposed: false });
+    }
+  }, [disposable, getState, hidden, setState]);
+
+  const { disposed, lastChildren } = getState();
+  const children = keepChildren ? childrenProp || lastChildren : childrenProp;
+
+  const css = useStyles({
+    classes: { root: className },
+    hidden: disposable ? hidden || disposed : hidden,
+    collapsable,
+    transitionDuration,
+    transitionTimingFunction,
   });
 
   if (hidden && disposable && disposed) return null;
@@ -87,6 +108,8 @@ export default function HideableFlex<C extends React.ElementType = DefaultCompon
       className={hidden && hiddenClassName ? `${css.root} ${hiddenClassName}` : css.root}
       {...rest}
       onTransitionEnd={transitionEndHandler}
+      // eslint-disable-next-line react/no-children-prop
+      children={children}
     />
   );
 }

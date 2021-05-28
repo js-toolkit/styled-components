1c015b7a799056fa44850c3a3b99e8e02d4ee267
Vladimir <vzh.inbox@gmail.com>
2021-05-28 16:26:09 +0600

    Improve useHideableState

--- src/HideableFlex/useHideableState.tsx
+++ src/HideableFlex/useHideableState.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import useRefCallback from '@js-toolkit/react-hooks/useRefCallback';
 
 export interface HideableState {
@@ -11,11 +11,23 @@ export type UseHideableStateProps = HideableState;
 export type UseHideableStateResult = HideableState & { hide: VoidFunction; disable: VoidFunction };
 
 export default function useHideableState(
-  initialState: HideableState | (() => HideableState)
+  initialState: HideableState | (() => HideableState),
+  stateUpdate?: (prevState: HideableState) => HideableState,
+  stateUpdateDeps: React.DependencyList = []
 ): UseHideableStateResult {
   const [state, setState] = useState<HideableState>(initialState);
+  const firstRenderRef = useRef(true);
   const stateRef = useRef(state);
   stateRef.current = state;
+
+  useEffect(() => {
+    if (firstRenderRef.current) {
+      firstRenderRef.current = false;
+      return;
+    }
+    stateUpdate && setState(stateUpdate);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, stateUpdateDeps);
 
   const hide = useRefCallback(() => {
     if (!state.visible) return;

42c5590f3370a379b67364c1b373c7e62e5beefc
Vladimir <vzh.inbox@gmail.com>
2022-03-09 12:27:46 +0600

    Updete deps and configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,6 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'react/prop-types': 'off',
+    'react/require-default-props': 'off',
   },
 };

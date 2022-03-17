135ae97bae775653096373e8d93188bd1aa5a189
Vladimir <vzh.inbox@gmail.com>
2022-03-17 12:48:42 +0600

    Add isReactElementOfType

---
+++ src/isReactElementOfType.ts
@@ -0,0 +1,34 @@
+/* eslint-disable dot-notation */
+import React from 'react';
+import ReactIs from 'react-is';
+
+export function isReactElementOfType<
+  T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>
+>(
+  element: React.ReactNode | React.ElementType<any>,
+  type: T,
+  deep = false
+): element is React.ReactComponentElement<T, React.ComponentProps<T>> {
+  if (React.isValidElement(element)) {
+    if (element['$$typeof'] === ReactIs.Memo) {
+      return isReactElementOfType(element['type'] as React.ElementType<any>, type, deep);
+    }
+
+    if (isReactElementOfType(element['type'] as React.ElementType<any>, type, deep)) {
+      return true;
+    }
+
+    if (
+      deep &&
+      React.Children.count((element.props as React.PropsWithChildren<unknown>).children) === 1
+    ) {
+      return isReactElementOfType(
+        React.Children.only((element.props as React.PropsWithChildren<unknown>).children),
+        type,
+        deep
+      );
+    }
+  }
+
+  return element === type;
+}

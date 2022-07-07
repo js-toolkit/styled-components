27a6f91e1425fbd63df7a54fb98d9c3b24db689e
Vladimir <vzh.inbox@gmail.com>
2022-07-07 12:51:41 +0600

    Improve TruncatedText

--- src/TruncatedText/TruncatedText.tsx
+++ src/TruncatedText/TruncatedText.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import makeStyles from '@mui/styles/makeStyles';
+import clsx from 'clsx';
 import { Flex, FlexAllProps } from 'reflexy';
+
+export type TruncatedTextProps<C extends React.ElementType = 'span'> = FlexAllProps<C> & {
+  lines?: number;
+};
+
+type MakeStylesProps = Pick<TruncatedTextProps, 'lines'>;
 
 const useStyles = makeStyles({
   root: {
@@ -8,15 +15,32 @@ const useStyles = makeStyles({
     overflow: 'hidden',
     textOverflow: 'ellipsis',
   },
-});
 
-export type TruncatedTextProps<C extends React.ElementType = 'span'> = FlexAllProps<C>;
+  multiline: {
+    whiteSpace: 'normal',
+    hyphens: 'auto',
+    display: '-webkit-box',
+    boxOrient: 'vertical',
+    lineClamp: ({ lines }: MakeStylesProps) => lines,
+  },
+});
 
 export default function TruncatedText<C extends React.ElementType = 'span'>({
   component = 'span' as C,
+  lines,
   className,
   ...rest
 }: TruncatedTextProps<C>): JSX.Element {
-  const css = useStyles({ classes: { root: className } });
-  return <Flex flex={false} component={component} className={css.root} {...(rest as any)} />;
+  const css = useStyles({
+    classes: { root: className },
+    lines: lines && lines > 0 ? lines : undefined,
+  });
+  return (
+    <Flex
+      flex={false}
+      component={component}
+      className={clsx(css.root, lines && lines > 0 && css.multiline)}
+      {...(rest as any)}
+    />
+  );
 }

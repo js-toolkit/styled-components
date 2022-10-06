97b0eab1f41bd50781c873f66ef147484b881ff6
Vladimir <vzh.inbox@gmail.com>
2022-10-06 10:17:20 +0600

    Improve TooltipButton

--- src/TooltipButton/TooltipButton.tsx
+++ src/TooltipButton/TooltipButton.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useRef } from 'react';
-import useToggleDebounce from '@jstoolkit/react-hooks/useToggleDebounce';
+import noop from '@jstoolkit/utils/noop';
+import debounce from '@jstoolkit/utils/debounce';
+import useRefs from '@jstoolkit/react-hooks/useRefs';
+import useMemoDestructor from '@jstoolkit/react-hooks/useMemoDestructor';
 import useRefCallback from '@jstoolkit/react-hooks/useRefCallback';
 import Button, { ButtonProps } from '../Button';
 
@@ -29,25 +32,63 @@ export default function TooltipButton<C extends React.ElementType = 'button', D 
   onHideTooltip,
   ...restProps
 }: TooltipButtonProps<C, D>): JSX.Element {
-  const { onClick, onMouseEnter, onMouseLeave, onTouchStart, onContextMenu, ...rest } =
-    restProps as TooltipButtonProps<'button'>;
+  const {
+    onClick,
+    onMouseEnter,
+    onMouseLeave,
+    onTouchStart,
+    onContextMenu,
+    componentRef,
+    ...rest
+  } = restProps as TooltipButtonProps<'button'>;
 
-  const [getHover, getHoverData, setHover] = useToggleDebounce<HTMLButtonElement | undefined>({
-    wait: tooltipDelay,
-  });
-  const isHover = getHover();
-  const element = getHoverData();
+  // const [getHover, getHoverData, setHover] = useToggleDebounce<HTMLButtonElement | undefined>({
+  //   wait: tooltipDelay,
+  // });
+  // const isHover = getHover();
+  // const element = getHoverData();
 
+  const rootRef = useRef<HTMLButtonElement>(null);
+  const rootRefs = useRefs(rootRef, componentRef);
+  const isShowingTooltipRef = useRef(false);
   /** To prevent mouseenter (showing tooltip) on touch */
   const isTouchingRef = useRef(false);
 
-  useEffect(() => {
-    if (isHover && onShowTooltip && tooltip && element) {
-      onShowTooltip({ target: element, title: tooltip, data });
-    } else if ((!isHover || !tooltip) && onHideTooltip && element) {
-      onHideTooltip(element);
+  const showTooltip = useRefCallback(() => {
+    if (rootRef.current && tooltip && onShowTooltip) {
+      isShowingTooltipRef.current = true;
+      onShowTooltip({ target: rootRef.current, title: tooltip, data });
     }
-  }, [data, element, isHover, onHideTooltip, onShowTooltip, tooltip]);
+  });
+
+  const hideTooltip = useRefCallback(() => {
+    if (rootRef.current && onHideTooltip) {
+      isShowingTooltipRef.current = false;
+      onHideTooltip(rootRef.current);
+    }
+  });
+
+  const hideTooltipDelayed = useMemoDestructor(() => {
+    if (onHideTooltip && tooltipDelay > 0) {
+      const fn = debounce(hideTooltip, tooltipDelay);
+      return [fn, () => fn.flush()];
+    }
+    return [hideTooltip, noop];
+  }, [hideTooltip, onHideTooltip, tooltipDelay]);
+
+  useEffect(() => {
+    if (!isShowingTooltipRef.current) return;
+    if (tooltip) showTooltip();
+    else hideTooltip();
+  }, [hideTooltip, showTooltip, tooltip]);
+
+  // useEffect(() => {
+  //   if (isHover && onShowTooltip && tooltip && element) {
+  //     onShowTooltip({ target: element, title: tooltip, data });
+  //   } else if ((!isHover || !tooltip) && onHideTooltip && element) {
+  //     onHideTooltip(element);
+  //   }
+  // }, [data, element, isHover, onHideTooltip, onShowTooltip, tooltip]);
 
   const touchStartHandler = useRefCallback<React.TouchEventHandler<HTMLButtonElement>>((event) => {
     // console.log('touchStart', event.nativeEvent.type);
@@ -70,19 +111,22 @@ export default function TooltipButton<C extends React.ElementType = 'button', D 
   const mouseEnterHandler = useRefCallback<React.MouseEventHandler<HTMLButtonElement>>((event) => {
     // console.log('mouseEnter', event.nativeEvent.type, event.nativeEvent);
     if (isTouchingRef.current) return;
-    tooltip && onShowTooltip && setHover(true, event.currentTarget);
+    // tooltip && onShowTooltip && setHover(true, event.currentTarget);
+    showTooltip();
     onMouseEnter && onMouseEnter(event);
   });
 
   const mouseLeaveHandler = useRefCallback<React.MouseEventHandler<HTMLButtonElement>>((event) => {
     // console.log('mouseLeave', event.nativeEvent.type, event.nativeEvent);
     if (isTouchingRef.current) return;
-    (getHover() || tooltip) && onHideTooltip && setHover(false, event.currentTarget);
+    // (getHover() || tooltip) && onHideTooltip && setHover(false, event.currentTarget);
+    hideTooltipDelayed();
     onMouseLeave && onMouseLeave(event);
   });
 
   return (
     <Button
+      componentRef={rootRefs}
       onMouseEnter={mouseEnterHandler}
       onMouseLeave={mouseLeaveHandler}
       onTouchStart={touchStartHandler}

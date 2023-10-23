794c7331a6cd16c839f63c4adeccde0aa3134749
Vladimir <vzh.inbox@gmail.com>
2023-10-23 12:14:57 +0600

    Add SvgForeignText

---
+++ src/svg/SvgForeignText.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import type { Size } from '@js-toolkit/utils/types/utils';
+import useRefState from '@js-toolkit/react-hooks/useRefState';
+
+export interface SvgForeignTextProps extends React.SVGProps<SVGForeignObjectElement> {
+  readonly children: string;
+}
+
+export default function SvgForeignText({
+  children: text,
+  ...rest
+}: SvgForeignTextProps): React.JSX.Element {
+  const [getSize, setSize] = useRefState<Size>(() => ({ width: 0, height: 0 }));
+  const contentRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    const content = contentRef.current;
+    if (!content) return;
+    setSize({ width: content.scrollWidth, height: content.scrollHeight });
+  }, [setSize]);
+
+  const { width, height } = getSize();
+
+  return (
+    <foreignObject x={0} y={0} width={width} height={height} {...rest}>
+      <div ref={contentRef}>{text}</div>
+    </foreignObject>
+  );
+}

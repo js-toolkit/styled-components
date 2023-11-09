c76bfb2887ffaf0a1430bd763b16c67e9b5c7551
Vladimir <vzh.inbox@gmail.com>
2023-11-09 12:20:37 +0600

    Refactor

--- src/isReactElementOfType.ts
+++ src/isReactElementOfType.ts
@@ -5,16 +5,16 @@ import ReactIs from 'react-is';
 export function isReactElementOfType<
   T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>,
 >(
-  element: React.ReactNode | React.ElementType<any>,
+  element: React.ReactNode | React.ComponentType<any>,
   type: T,
   deep = false
 ): element is React.ReactComponentElement<T, React.ComponentProps<T>> {
   if (React.isValidElement(element)) {
     if ((element as AnyObject)['$$typeof'] === ReactIs.Memo) {
-      return isReactElementOfType(element['type'] as React.ElementType<any>, type, deep);
+      return isReactElementOfType(element['type'] as React.ElementType, type, deep);
     }
 
-    if (isReactElementOfType(element['type'] as React.ElementType<any>, type, deep)) {
+    if (isReactElementOfType(element['type'] as React.ElementType, type, deep)) {
       return true;
     }
 

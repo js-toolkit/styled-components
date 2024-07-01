eeb12802ca1666c3ef4fc66d98b832c42f32a644
Vladimir <vzh.inbox@gmail.com>
2024-07-01 12:02:29 +0500

    Update eslint

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = [
+  ...require('@js-toolkit/configs/eslint/react'),
+  ...require('@js-toolkit/react-hooks/eslint'),
+
+  {
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'react/prop-types': 'off',
+      'react/require-default-props': 'off',
+    },
+  },
+];

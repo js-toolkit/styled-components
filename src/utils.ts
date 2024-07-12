93517eda0a339d93989484368aa60b3c5626501f
Vladimir <vzh.inbox@gmail.com>
2024-07-12 17:29:39 +0500

    Improve excludeProp

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import type { StyledOptions } from '@mui/styled-engine';
 
-export function excludeProp<T extends string>(
-  list: readonly T[]
+export function excludeProp<T extends string | AnyObject, L = T extends string ? T : keyof T>(
+  list: readonly L[]
 ): NonNullable<StyledOptions['shouldForwardProp']> {
   return (prop) => !(list as readonly string[]).includes(prop);
 }

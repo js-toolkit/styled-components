411562326510cb5c7256908ea24581623526771d
Vladimir <vzh.inbox@gmail.com>
2024-12-19 16:18:37 +0500

    Improve Poster

--- src/Poster/Poster.tsx
+++ src/Poster/Poster.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import useRefCallback from '@js-toolkit/react-hooks/useRefCallback';
 import useHideableState from '@js-toolkit/react-hooks/useHideableState';
+import useMemoDestructor from '@js-toolkit/react-hooks/useMemoDestructor';
 import Picture, { type PictureProps } from '../Picture';
 
 export interface PosterProps extends PictureProps {
@@ -17,18 +17,39 @@ export default function Poster({
   onHidden,
   ...rest
 }: PosterProps): React.JSX.Element {
-  const state0 = useHideableState(() => ({ enabled: !!src0, visible: !!src0 }), [src0]);
+  const state0 = useHideableState(
+    () => ({ enabled: !!(src && src0), visible: !!(src && src0) }),
+    [src, src0]
+  );
 
-  const shownHandler = useRefCallback(() => {
-    state0.hide();
-    onShown && onShown();
-  });
+  const shownHandler = useMemoDestructor(() => {
+    let timer = 0;
+    return [
+      () => {
+        // Use src0 just for deps correct linting.
+        if (src && src0) {
+          // In order to avoid a blink on hide level0 img.
+          timer = setTimeout(state0.hide, 100);
+        }
+        onShown && onShown();
+      },
+      () => clearTimeout(timer),
+    ];
+  }, [onShown, src, src0, state0.hide]);
 
   const hasLevel0 = !!(src0 && state0.enabled);
 
-  // Order has matter in order to avoid a blink on hide l0 img.
   return (
     <>
+      {hasLevel0 && (
+        <Picture
+          src={src0}
+          hidden={hidden || state0.hidden}
+          timeout={timeout}
+          onHidden={state0.disable}
+          {...rest}
+        />
+      )}
       <Picture
         hidden={hidden || (hasLevel0 ? undefined : hidden)}
         src={src}
@@ -37,16 +58,6 @@ export default function Poster({
         onHidden={onHidden}
         {...rest}
       />
-      {hasLevel0 && (
-        <Picture
-          src={src0}
-          hidden={hidden || state0.hidden}
-          timeout={timeout}
-          transitionProps={{ easing: { enter: 'ease-in', exit: 'linear' } }}
-          onHidden={state0.disable}
-          {...rest}
-        />
-      )}
     </>
   );
 }
